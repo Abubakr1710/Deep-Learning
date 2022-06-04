a863bb7132ba1fef2b11c321c75d7e33650109a2
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-06-03 23:14:51 -0700

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,26 @@
+from pickle import TRUE
 import pandas as pd
 import numpy as np
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-torch.random.seed(0)
+torch.manual_seed(0)
 
-def transformer():
+def dataloader():
+
     transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5),(0.5))])
-    return transform
+    
+    trainset=datasets.MNIST('MNIST_data/', download=True, train=True, transform=transform)
+    trainloader=DataLoader(trainset, batch_size=32, shuffle=True)
 
+    testset=datasets.MNIST('MNIST_data', download=True, train=False, transform=transform)
+    testloader=DataLoader(testset, batch_size=32, shuffle=False)
+
+    return trainloader, testloader
+
+trainloader, testloader=dataloader()
+
+dataiter=iter(trainloader)
+images, labels=dataiter.next()
+print(images.shape)
+print(labels.shape)
