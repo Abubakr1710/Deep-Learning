e5bf9ed7b5626912c2933a84035c7e156f4c999e
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-06-03 23:01:01 -0700

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
 import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+torch.random.seed(0)
+
+def transformer():
+    transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5),(0.5))])
+    return transform
+
